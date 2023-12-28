953c7819161a9985f3dbe7f9f0e0ffce67f7e13e
Faraz Khan <farazkhanaa2555@gmail.com>
2023-12-28 15:36:17 +0500

    feat: Display User History

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,19 +1,19 @@
 export const addTwoNumbers = (number1: number, number2: number) => {
-  const sum = number1 + number1;
-  return sum;
+    const sum = number1 + number1;
+    return sum;
 };
 
 export const subtractTwoNumbers = (number1: number, number2: number) => {
-  const subtract = number1 - number1;
-  return subtract;
+    const subtract = number1 - number1;
+    return subtract;
 };
 
 export const multipleTwoNumbers = (number1: number, number2: number) => {
-  const multiply = number1 * number1;
-  return multiply;
+    const multiply = number1 * number1;
+    return multiply;
 };
 
 export const divideTwoNumber = (number1: number, number2: number) => {
-  const divide = number1 / number1;
-  return divide;
+    const divide = number1 / number1;
+    return divide;
 };
